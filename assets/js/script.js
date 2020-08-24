6d09f93f772ac4832d378f0fc42926c8aaa0893c
danikrs <danikrs8@gmail.com>
2020-08-24 09:13:48 +0000

    added back to reading function

--- assets/js/script.js
+++ assets/js/script.js
@@ -42,3 +42,10 @@ $("img.card-back").on({
   });
 });
 
+function backToReading() {
+  $(".btn-warning").addClass("d-none")
+  $(".game").removeClass("col-lg-4");
+  $(".card-info").addClass("d-none");
+  $(".card-image").addClass("d-none");
+}
+
