3f3e977e1c197d83061400a52f347818419eef43
danikrs <danikrs8@gmail.com>
2020-09-26 07:35:25 +0000

    added font awesome, edited text, changed color on buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -27,7 +27,7 @@ function descriptionClick(index) { //this is to retrive the card info from the c
     $(".card-container").addClass("card-container2");
     $(".card-meaning").addClass("card-meaning2");
     document.querySelector(".info").innerHTML = clickedCard.description;
-    document.querySelector("#card-name").innerHTML = `<i class="fas fa-caret-down"></i> &nbsp; ${clickedCard.name} &nbsp; <i class="fas fa-caret-up"></i>`;
+    document.querySelector("#card-name").innerHTML = `<i class="fab fa-centos"></i> &nbsp; ${clickedCard.name} &nbsp; <i class="fab fa-centos"></i>`;
     document.querySelector(".image").innerHTML = `<img src="${clickedCard.imgPath}" alt="front-card">`;
 }
 
